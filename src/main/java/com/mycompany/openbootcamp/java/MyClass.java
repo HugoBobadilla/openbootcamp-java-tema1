67ab18055d862bc29ce02d97389b64bcbbf0bd1c
HugoBobadilla <hugoabobadilla1998@gmail.com>
2022-07-21 22:01:00 -0400

    Ejercicios tema 1

---
+++ src/main/java/com/mycompany/openbootcamp/java/MyClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.openbootcamp.java;
+
+/**
+ *
+ * @author hugo
+ */
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println(20);
+        System.out.println(12345678910L);
+        System.out.println(6.5);
+        System.out.println(true);
+        System.out.println("Hola Mundo!");
+    }
+}
